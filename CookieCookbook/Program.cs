4d34779a7a204b12cf9ccd51a192a36ea57b8c4b
Zach Waller <zachjives@gmail.com>
2024-07-01 21:21:59 -0400

    Add project files.

---
+++ CookieCookbook/Program.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Runtime.CompilerServices;
+
+var cookiesRecipesApp = new CookiesRecipesApp(
+    new RecipesRepository(),
+    new RecipesConsoleUserInteraction());
+
+cookiesRecipesApp.Run();
+
+public class CookiesRecipesApp
+{
+    private readonly IRecipesRepository _recipesRepository;
+    private readonly IRecipesUserInteraction _recipesUserInteraction;
+
+    public CookiesRecipesApp(
+        IRecipesRepository receipesRepository,
+        IRecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = receipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+    public void Run()
+    {
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+        _recipesUserInteraction.PromptToCreateRecipe();
+
+        var ingredients = _recipesUserInteraction.ReadIngredientsForUser;
+
+        if (ingredients.Count > 0)
+        {
+            var recipes = new Recipe(ingredients);
+            allRecipes.Add(recipe);
+            _recipesRepository.Write(filePath, allRecipes);
+
+            _recipesUserInteraction.ShowMessage("Recipe added:");
+            _recipesUserInteraction.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _recipesUserInteraction.ShowMessage(
+                "No ingredients have been selected. " +
+                "Recipe will not be saved.");
+        }
+
+        _recipesUserInteraction.Exit();
+    }
+}
+
+public interface IRecipesUserInteraction
+{
+    void ShowMessage(string message);
+    void Exit();
+
+}
+public class RecipesConsoleUserInteraction : IRecipesUserInteraction
+{
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
+
+public interface IRecipesRepository
+{
+
+}
+
+public class RecipesRepository : IRecipesRepository
+{
+
+}
